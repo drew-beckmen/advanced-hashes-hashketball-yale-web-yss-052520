d8be15d80dcb9b5dbea99f5d18540801b5b2dc25
Drew Beckmen <drew.beckmen@yale.edu>
2020-05-11 20:26:06 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -133,6 +133,7 @@ def num_points_scored(player_name)
       if attribute == :players
         val.each do |player|
           points = player.select {|x| x[player_name]}
+        end 
       end
     end
   end
